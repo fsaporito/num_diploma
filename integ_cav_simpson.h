9abc83ad8486c1d1ad420a2489f62b3d40b2b786
sapo93 <sapo93@gmail.com>
2013-11-15 22:57:29 +0100

    Create integ_cav_simpson.h
    Cavalieri Simpson Method

---
+++ integ_cav_simpson.h
@@ -0,0 +1,16 @@
+/* integ_cav_simpson.h */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "function.h"
+#include "deriv4.h"
+
+// *param ---------> array with the coefficient of the function
+// dimension ------> coefficient number (param dimension)
+// functiontype ---> function type
+// errorinteg ----------> error done while computing the integral 
+// extr_a  --------> first extreme point
+// extr_b ---------> second exreme point
+
+inline double integ_cav_simpson (double *param, int dimension, int functiontype, double *errorinteg, int conv, double extr_a, double extr_b);
