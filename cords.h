a941ac593b99dd9123d554967139b3925b8f0ae9
sapo93 <sapo93@gmail.com>
2013-11-15 23:07:10 +0100

    Create cords.h
    Cords Header

---
+++ cords.h
@@ -0,0 +1,18 @@
+/* cords.h */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <math.h>
+#include "function.h"
+
+// *param ---------> array with the coefficient of the function
+// dimension ------> coefficient number (param dimension)
+// functiontype ---> function type
+// precision ------> precision reuired
+// error ----------> error done while computing the root
+// inter ----------> number of executed iterations
+// extr_a  --------> first extreme point
+// extr_b ---------> second exreme point
+
+inline double cords (double *param, int dimension, int functiontype, double precision, double *error, long int *inter, double extr_a, double extr_b, int mode);
