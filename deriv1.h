6c937d033e25ae84df2d4c4affc09a77bf5f0764
sapo93 <sapo93@gmail.com>
2013-11-15 23:05:29 +0100

    Create deriv1.h
    First Derivate Header

---
+++ deriv1.h
@@ -0,0 +1,13 @@
+/* deriv1.h */
+
+#include <stdio.h>
+#include <math.h>
+
+#define  e = 2.7182818284
+
+// *param ---------> array with the coefficient of the function
+// dimension ------> coefficient number (param dimension)
+// functiontype ---> function type
+// x --------------> indipendent variable
+
+inline double deriv1 (double *param, int dimension, int functiontype, double x);
