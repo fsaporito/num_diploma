08d1b8cdccde7957c782671e1c2bf57ddcd40fb7
sapo93 <sapo93@gmail.com>
2013-11-15 22:59:20 +0100

    Create function.h
    Function Header

---
+++ function.h
@@ -0,0 +1,13 @@
+/* function.h */
+
+#include <stdio.h>
+#include <math.h>
+
+#define  e = 2.7182818284
+
+// *param ---------> array with the coefficient of the function
+// dimension ------> coefficient number (param dimension)
+// functiontype ---> function type
+// x --------------> indipendent variable
+
+inline double function (double *param, int dimension, int functiontype, double x);
